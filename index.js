86da040efb47dc96bf8d36cbef23bc9b4b53d443
Saisurya93 <135871669+Saisurya93@users.noreply.github.com>
2023-06-13 17:40:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const addDays = require("date-fns/addDays");
 
 const getDaysAfterXDays = (days) => {
-  const newDate = addDays(new Date(2020, 8, 22), days);
-  console.log(`${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`);
+  const newDate = addDays(new Date(2020, 7, 22), days);
+  console.log(`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`);
 };
 
 module.exports = getDaysAfterXDays;
